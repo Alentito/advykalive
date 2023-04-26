7926e40286cc7325a4ae5bb4164fa73a0e726bfb
Alen Tito <61512914+Alentito@users.noreply.github.com>
2023-04-26 10:58:40 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,12 @@
 // images setup
 const image = [
-  "./img/1.jpg",
-  "./img/2.jpg",
-  "./img/3.jpg",
-  "./img/4.jpg",
-  "./img/5.jpg",
+  "./img/1.png",
+  "./img/2.png",
+  "./img/3.png",
 ];
 
 // content setup
 const texts = [
-  ["Register Now"],
-  ["Register Now"],
   ["Register Now"],
   ["Register Now"],
   ["Register Now"],
